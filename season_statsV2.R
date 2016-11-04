cf18eb11ae79553cb07bc8731e0f52bc0532f370
awunderground <williamsar6@vcu.edu>
2016-11-04 08:50:09 -0400

    Create list with dfs season season1972 to season2009. Add a function that cleans 1st row and creates more uniform names

--- season_statsV2.R
+++ season_statsV2.R
@@ -66,7 +66,7 @@ statScraperA <- function (link) {
         html_text()
     
     if (length(vector11) == 0) {
-        vector11 <- rep(NA, length(vector10))
+        vector11 <- rep("vector11", length(vector10))
     }
 
     vector12 <- temp %>% 
@@ -74,7 +74,7 @@ statScraperA <- function (link) {
         html_text()
     
     if (length(vector12) == 0) {
-        vector12 <- rep(NA, length(vector11))
+        vector12 <- rep("vector12", length(vector11))
     }
     
     vector13 <- temp %>% 
@@ -82,7 +82,7 @@ statScraperA <- function (link) {
         html_text()
     
     if (length(vector13) == 0) {
-        vector13 <- rep(NA, length(vector12))
+        vector13 <- rep("vector13", length(vector12))
     }
     
     vector14 <- temp %>% 
@@ -90,7 +90,7 @@ statScraperA <- function (link) {
         html_text()
     
     if (length(vector14) == 0) {
-        vector14 <- rep(NA, length(vector13))
+        vector14 <- rep("vector14", length(vector13))
     }
     
     vector15 <- temp %>% 
@@ -98,7 +98,7 @@ statScraperA <- function (link) {
         html_text()
     
     if (length(vector15) == 0) {
-        vector15 <- rep(NA, length(vector14))
+        vector15 <- rep("vector15", length(vector14))
     }
     
     vector16 <- temp %>% 
@@ -106,7 +106,7 @@ statScraperA <- function (link) {
         html_text()
     
     if (length(vector16) == 0) {
-        vector16 <- rep(NA, length(vector15))
+        vector16 <- rep("vector16", length(vector15))
     }
     
     vector17 <- temp %>% 
@@ -114,7 +114,7 @@ statScraperA <- function (link) {
         html_text()
     
     if (length(vector17) == 0) {
-        vector17 <- rep(NA, length(vector16))
+        vector17 <- rep("vector17", length(vector16))
     }
     
     boom <- data_frame(link, player, games.played, vector3, vector4, vector5, 
@@ -133,16 +133,47 @@ for (i in 1:nrow(stats)) {
     
 }
 
+seasons <- list(season1972, season1973, season1974, season1975, season1976, 
+                season1977, season1978, season1979, season1980, season1981,
+                season1982, season1983, season1984, season1985, season1986,
+                season1987, season1988, season1989, season1990, season1991,
+                season1992, season1993, season1994, season1995, season1996,
+                season1997, season1998, season1999, season2000, season2001,
+                season2002, season2003, season2004, season2005, season2006,
+                season2007, season2008, season2009)
+
+namer <- function(df) {
+
+    df[1, ] <- trimws(df[1, ])
+    df[1, ] <- tolower(df[1, ])
+    df[1, ] <- gsub("\\.", "", df[1, ])
+    df[1, ] <- gsub("-", ".", df[1, ])
+
+    names(df) <- df[1, ]
+    df <- df[2:nrow(df), ]
+    
+    return(df)
+}
+
+seasons <- lapply(seasons, namer)
 
 
 
 
 
+# player - good 
+# games.played - good 
+# minutes
+# field.goals
+# three.pointers
+# free.throws
+# rebounds
+# assists
+# turnovers
+# blocks
+# steals
+# points
 
-
-td:nth-child(10)
-
-td:nth-child(17)
 
 
 
