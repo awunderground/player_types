a31b1b1c37684b11fee7773be142b59b6c71330f
awunderground <williamsar6@vcu.edu>
2016-11-07 08:32:58 -0500

    Add script to paste together copied-and-pasted .csv files for 2010-2016 seasons. Later, this data will be scraped from individual player pages.

---
+++ organize_recent_player_data.R
@@ -0,0 +1,50 @@
+library(stringr)
+library(tidyverse)
+
+year <- 2010:2016
+season <- rep("season", length(year))
+key <- paste0("data/", year, season, ".csv")
+
+long <- data_frame()
+for (i in 1:length(key)) {
+    
+    temp <- read.csv(key[i], header = TRUE, stringsAsFactors = FALSE)
+    
+    long <- bind_rows(long, temp)
+    
+}
+
+boom <- long %>%
+    select(-Rk, -FG., -X2P, -X2PA, -X2P., -X3P., -FT.) %>%
+    rename(player = Player, 
+           games.played = G, 
+           minutes = MP, 
+           field.goal = FG, 
+           field.goal.attempts = FGA,
+           three.pointers = X3P,
+           three.point.attempts = X3PA,
+           free.throws = FT,
+           free.throw.attempts = FTA,
+           offensive.rebounds = ORB,
+           defensive.rebounds = DRB,
+           total.rebounds = TRB,
+           assists = AST, 
+           steals = STL,
+           blocks = BLK,
+           turnovers = TOV,
+           fouls = PF,
+           points = PTS)
+
+
+
+
+
+link	player	games.played minutes	fg.fga	three.pointers	ft.fta	reb	pf.fo	ast	to	blk	stl	pts.avg
+
+
+
+
+boom <- 
+
+
+
