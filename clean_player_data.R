e4a31ca5d0ceecbdcfb98403de05d22515b11b0b
awunderground <williamsar6@vcu.edu>
2016-11-08 09:31:10 -0500

    Add gsub with regex in addition to trimws for removing extraneous white spaces

--- clean_player_data.R
+++ clean_player_data.R
@@ -7,18 +7,27 @@ recent.seasons <- read.csv("data/recent_longitudinal_player_stats.csv", header =
 old.seasons <- tbl_df(old.seasons)
 recent.seasons <- tbl_df(recent.seasons)
 
-boom <- old.seasons %>%
+# Clean up all cells and seasons
+old.seasons <- old.seasons %>%
     mutate_all(funs(gsub), pattern = "\n", replacement = "") %>%
     mutate_all(funs(trimws)) %>%
+    mutate_all(funs(gsub), pattern = "^\\s+", replacement = "") %>%
     mutate(season = substr(link, 50, 51)) %>%
     mutate(season = as.numeric(season)) %>%
-    mutate(season = ifelse(season > 17, season + 1900, season + 2000)) %>%
+    mutate(season = ifelse(season > 17, season + 1900, season + 2000)) 
+
+
+# Clean up games played and minutes
+old.seasons <- old.seasons%>%
     separate(games.played, sep = "-", into = c("games.played", "games.started")) %>%
     mutate(games.played = as.numeric(games.played)) %>%
     mutate(games.played = as.numeric(games.played)) %>%
     separate(minutes, sep = "-", into = c("minutes", "average.minutes")) %>%
     mutate(minutes = as.numeric(minutes)) %>%
-    mutate(average.minutes = as.numeric(average.minutes)) %>%
+    mutate(average.minutes = as.numeric(average.minutes)) 
+
+# Clean up field goals, three points, and free throws
+old.seasons <- old.seasons%>%
     separate(fg.fga, sep = "-", into = c("field.goals", "field.goal.attempts")) %>%
     mutate(field.goals = as.numeric(field.goals)) %>%
     mutate(field.goal.attempts = as.numeric(field.goal.attempts)) %>%
@@ -27,11 +36,25 @@ boom <- old.seasons %>%
     mutate(three.point.attempts = as.numeric(three.point.attempts)) %>%
     separate(ft.fta, sep = "-", into = c("free.throws", "free.throw.attempts")) %>%
     mutate(free.throws = as.numeric(free.throws)) %>%
-    mutate(free.throw.attempts = as.numeric(free.throw.attempts)) %>%
+    mutate(free.throw.attempts = as.numeric(free.throw.attempts))
+
+# Clean up rebounds and fouls
+boom <- old.seasons %>%
     mutate(total.rebounds = ifelse(grepl("-", reb), NA, reb)) %>%
     separate(reb, sep = "-", into = c("offensive.rebounds", "defensive.rebounds", "total.temp")) %>%
     mutate(total.rebounds = ifelse(is.na(total.rebounds), total.temp, total.rebounds)) %>%
-    mutate(offensive.rebounds = ifelse(is.na(defensive.rebounds), NA, offensive.rebounds))
+    mutate(offensive.rebounds = ifelse(is.na(defensive.rebounds), NA, offensive.rebounds)) %>%
+    separate(pf.fo, sep = "-", into = c("fouls", "foul.outs")) %>%
+    mutate(fouls = as.numeric(fouls)) %>%
+    mutate(foul.outs = as.numeric(foul.outs))
+    
+# Clean up assists, turnovers, blocks, and steals
+
+
+
+
+
+
 
 zoom <- mutate(boom, zam = ifelse(total.rebounds == NA, total.temp, total.rebounds)) %>%
     select(total.rebounds)
