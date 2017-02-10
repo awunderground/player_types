0a34a69a535fad8e7262139daee2ba0d2a3fb799
awunderground <aaronrwilliams100@gmail.com>
2017-02-09 23:02:22 -0500

    Finalize images and tables

--- girth/girth.R
+++ girth/girth.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+library(htmlTable)
 
 options(scipen = 999)
 
@@ -32,7 +33,7 @@ players <- union(players, players2017)
 filter(players, !is.na(height)) # 424 of 585
 filter(players, !is.na(weight)) # 424 of 585
 
-
+# Calculate the % of players in each season with height and weight
 data.summary <- players %>%
     group_by(season) %>%
     mutate(dummy = ifelse(!is.na(height) & !is.na(weight), 1, 0)) %>%
@@ -40,12 +41,13 @@ data.summary <- players %>%
 
 # Data
 ggplot(data.summary, aes(season, values)) +
-    geom_bar(stat = "identity") +
-    labs(title = "Proportion of Players With Height and Weight by Season",
-         caption = "585 Player Years from 1972-Present") +
-    ylab("Percentage of Values") +
+    geom_bar(stat = "identity", fill = "gold1") +
+    labs(title = "Recent Rosters Are More Complete",
+         subtitle = "Proportion of Players With Height and Weight by Season",
+         caption = "424 Player Years from 1972-Present") +
+    ylab("Proportion of Values") +
     xlab("Season")
-
+ggsave("girth/complete_values.png", height = 6, width = 9, units = "in")
 
 # Height
 players <- players %>%
@@ -56,22 +58,55 @@ players <- players %>%
     mutate(girth.rank = rank(-pounds.per.inch, ties = "min"))
 
 # Height histogram
-ggplot(players, aes(height.inches, fill = ifelse(height.inches == 79, "gold", "grey4"))) +
+ggplot(players, aes(height.inches, fill = ifelse(height.inches == 79, "gold1", "grey4"))) +
     geom_histogram(binwidth = 1) +
     theme(legend.position="none") +
     ylab("Count") +
     xlab("Height (Inches)") +
-    labs(title = "Histogram of Player Heights")
+    labs(title = "Mo Alie-Cox is a Normal Height",
+         subtitle = "Histogram of VCU Men's Basketball Player Heights",
+         caption = "424 Player Years from 1972-Present")
+ggsave("girth/height_histogram.png", height = 6, width = 9, units = "in")
 
-# Weight 
+# Weight histogram
 ggplot(players, aes(weight, fill = ifelse(weight == 250, "gold", "grey4"))) +
     geom_histogram(binwidth = 5) +
     theme(legend.position="none") +
     ylab("Count") +
     xlab("Weight (Pounds)") +
-    labs(title = "Histogram of Player Weights")
+    labs(title = "Mo Alie-Cox is an Abnormal Weight",
+         subtitle = "Histogram of VCU Men's Basketball Player Weights",
+         caption = "424 Player Years from 1972-Present")
+ggsave("girth/weight_histogram.png", height = 6, width = 9, units = "in")
 
-# height weight scatter plot
+# height-weight scatter plot
+ggplot(players, aes(height.inches, weight)) +
+    geom_jitter(size = 4, alpha = 0.5, color = "gold1") +
+    geom_text(aes(label = ifelse(season == 2017, as.character(last.name), "")), size = 3) +
+    ylab("Weight (Pounds)") +
+    xlab("Height (Inches)") +
+    labs(title = "Mo Alie-Cox is a Big Dude",
+         subtitle = "Heights and Weights of VCU Men's Basketball Players",
+         caption = "424 Player Years from 1972-Present")
+ggsave("girth/weight_height_scatterplot.png", height = 6, width = 9, units = "in")
 
+# html table of top players
+mytable1 <- players %>%
+    filter(girth.rank <= 10) %>%
+    arrange(girth.rank) %>%
+    mutate(Name = paste(first.name.x, last.name, sep = " ")) %>%
+    mutate(pounds.per.inch = round(pounds.per.inch, 2)) %>%
+    select(girth.rank, Season = season, Name, Weight = weight, Height = height, PoundsPerInch = pounds.per.inch)
 
+mytableout <- htmlTable(mytable1, rnames = FALSE)
+print(mytableout, type="html", useViewer=TRUE)
 
+# html table of bottom players
+mytable2 <- players %>%
+    filter(girth.rank >= 415) %>%
+    arrange(-girth.rank) %>%
+    mutate(pounds.per.inch = round(pounds.per.inch, 2)) %>%
+    select(girth.rank, Season = season, last.name, first.name.x, Weight = weight, Height = height)
+
+mytableout <- htmlTable(mytable2, rnames = FALSE)
+print(mytableout, type="html", useViewer=TRUE)
